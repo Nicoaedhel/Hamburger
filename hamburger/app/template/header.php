c5e3ce4bf333663c24dfef12c850a27c69bcc481
Nicoaedhel <nicoa.cn@gmail.com>
2015-04-10 08:02:32 +0200

    Edit css

--- hamburger/app/template/header.php
+++ hamburger/app/template/header.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?= $my_hamburger['title_blog']; ?></title>
-    <link rel="stylesheet" href="<?= $path; ?>hamburger/public/css/app.css" />
     <link rel="stylesheet" href="<?= $path; ?>hamburger/public/css/rrssb.css" />
+    <link rel="stylesheet" href="<?= $path; ?>hamburger/public/css/app.css" />
     <script src="<?= $path; ?>hamburger/public/assets/modernizr/modernizr.js"></script>
   </head>
   <body>
